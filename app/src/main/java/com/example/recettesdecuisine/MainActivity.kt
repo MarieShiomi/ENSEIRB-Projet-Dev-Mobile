292c9dc35b81f73dc0216f54664f5afe8b29149e
Marie <luigisy66@gmail.com>
2022-11-22 15:02:32 +0100

    Ajout du squelette de projet Android Studio

---
+++ app/src/main/java/com/example/recettesdecuisine/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.recettesdecuisine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
