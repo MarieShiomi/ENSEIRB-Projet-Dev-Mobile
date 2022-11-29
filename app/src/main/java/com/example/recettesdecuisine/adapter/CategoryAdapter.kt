4d1eeb9c924c48b1f7bef9a39d261805e4b90011
Marie <luigisy66@gmail.com>
2022-11-29 15:23:42 +0100

    Ajout d'un chargeur d'image ; Connexion avec l'api ; Visualisation des catégories avec un Adaptateur convenable

--- app/src/main/java/com/example/recettesdecuisine/adapter/CategoryAdapter.kt
+++ app/src/main/java/com/example/recettesdecuisine/adapter/CategoryAdapter.kt
@@ -1,8 +1,43 @@
 package com.example.recettesdecuisine.adapter
 
+import android.provider.ContactsContract.CommonDataKinds.Im
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recettesdecuisine.R
+import com.example.recettesdecuisine.formatdonnee.Category
+import com.example.recettesdecuisine.loader.ImageLoader
+import org.w3c.dom.Text
 
-class CategoryAdapter(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var categoryName: TextView = itemView.findViewById(R.id.category_name)
+    var categoryDesc: TextView = itemView.findViewById(R.id.category_desc)
+    var categoryImage: ImageView = itemView.findViewById(R.id.category_image)
+
+}
+
+class CategoryAdapter(val categories: List<Category>) : RecyclerView.Adapter<CategoryViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.itemcategory, parent, false)
+        return CategoryViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        holder.categoryName.setText(categories.get(position).name)
+        holder.categoryDesc.setText(categories.get(position).description)
+        ImageLoader.loader(categories.get(position).thumb, holder.categoryImage)
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return categories.count()
+    }
+
 }
---
+++ app/src/main/java/com/example/recettesdecuisine/loader/ImageLoader.kt
@@ -0,0 +1,42 @@
+package com.example.recettesdecuisine.loader
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Looper
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import android.os.Handler
+import android.view.View
+import android.widget.ImageView
+import java.util.BitSet
+
+class ImageLoader {
+
+
+    companion object {
+        private var executor: Executor = Executors.newSingleThreadExecutor()
+        private var handler = Handler(Looper.getMainLooper())
+        private var image: Bitmap? = null
+        fun loader(imageURL: String?, imageView: ImageView) {
+
+            executor.execute {
+                try {
+                    val inp = java.net.URL(imageURL).openStream()
+                    image = BitmapFactory.decodeStream(inp)
+
+                    handler.post {
+                        imageView.setImageBitmap(image)
+                    }
+                }
+                catch (e: java.lang.Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+
+
+
+
+}
