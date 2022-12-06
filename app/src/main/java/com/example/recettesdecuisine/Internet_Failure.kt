d49aa935a8ae349a4c4ae1098e237bb740d2d613
AsuyaT <bellapollux@gmail.com>
2022-12-06 15:56:28 +0100

    Ajout barre chargement et gestion non internet

---
+++ app/src/main/java/com/example/recettesdecuisine/Internet_Failure.kt
@@ -0,0 +1,24 @@
+package com.example.recettesdecuisine
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class Internet_Failure : AppCompatActivity() {
+    private lateinit var textview : ImageView
+    private lateinit var button : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //textview = findViewById(R.id.textview_id)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_internet_failure)
+        button = findViewById(R.id.button_id)
+        button.setOnClickListener{
+            var a = Intent(this, MainActivity::class.java).apply {
+            }
+            startActivity(a)
+        }
+    }
+}
--- app/src/main/java/com/example/recettesdecuisine/MainActivity.kt
+++ app/src/main/java/com/example/recettesdecuisine/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.recettesdecuisine
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recettesdecuisine.adapter.CategoryAdapter
@@ -12,13 +14,14 @@ import com.google.gson.Gson
 import okhttp3.*
 import java.io.IOException
 import java.net.URL
-
+import com.google.android.material.progressindicator.CircularProgressIndicator
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var recyclerView: RecyclerView
     private lateinit var categoriesAdapter: CategoryAdapter
+    private lateinit var circularProgressIndicator: CircularProgressIndicator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         recyclerView = binding.recyclerView
         //setContentView(R.layout.activity_main)
-
+        circularProgressIndicator = findViewById(R.id.circular_progress_indicator)
         val url = URL("https://www.themealdb.com/api/json/v1/1/categories.php")
 
         val request = Request.Builder()
@@ -34,11 +37,17 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         val client = OkHttpClient()
+        val internetFailure = Intent(this, Internet_Failure::class.java).apply {
+
+        }
+        circularProgressIndicator.visibility = View.VISIBLE
 
         client.newCall(request).enqueue(object : Callback {
 
             override fun onFailure(call: Call, e: IOException) {
                 Log.e("OKHTTP", e.localizedMessage)
+                circularProgressIndicator.visibility = View.GONE
+              startActivity(internetFailure)
             }
 
             override fun onResponse(call: Call, response: Response) {
@@ -47,6 +56,7 @@ class MainActivity : AppCompatActivity() {
                     val categoriesResponse = gson.fromJson(it, CategoryResponse::class.java)
                     categoriesResponse.categories?.let { it1 ->
                         runOnUiThread {
+                            circularProgressIndicator.visibility = View.GONE
                             categoriesAdapter = CategoryAdapter(it1)
                             recyclerView.adapter = categoriesAdapter
                             recyclerView.layoutManager = LinearLayoutManager(applicationContext)
